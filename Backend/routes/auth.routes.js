14f4f7f442fecdb2a5dd1a441d4c6368dcea8bc6
Sneha-Singh21 <indsneha2002@gmail.com>
2025-01-26 18:38:06 +0530

    Authentication implemented

--- Backend/routes/auth.routes.js
+++ Backend/routes/auth.routes.js
@@ -1,6 +1,12 @@
 import express from "express";
+import { getMe, login, logout, signup } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/protectRoute.js";
 
-const router = express.Router();
+const userRouter = express.Router();
 
+userRouter.get("/me",protectRoute, getMe)
+userRouter.post("/signup", signup)
+userRouter.post("/login", login)
+userRouter.post("/logout", logout)
 
-export default router;
+export default userRouter;
--- Backend/server.js
+++ Backend/server.js
@@ -3,12 +3,20 @@ import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// middleware
+app.use(express.json()); //to parse req.body (formdata)
+app.use(express.urlencoded({extended: true})); // to parse formdata(urlencoded)
+app.use(cookieParser());
+
+
+// api endpoints
 app.use("/api/auth",authRoutes);
 
 app.listen(PORT, () => {
